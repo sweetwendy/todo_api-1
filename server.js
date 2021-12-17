f0d2150a0afb6af210c25a45ca7ebb9b1bbd10a8
s-muel <samuel.essuman@amalitech.org>
2021-12-17 10:52:58 +0000

    added a script to start the server on heroku

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const { response, request } = require('express');
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
 //importing mongoose 
 const mongoose = require('mongoose');
 // importing todoController
 const todoController = require('./controllers/todoController');
 
 app.use(express.json());
+
+app.get('/', (request,response)=>{
+response.status(200).json({message:"Hello Welcome to my todo API"});
+});
   
 app.post('/todo',todoController.addTodo);
 app.get ('/todo',todoController.getAllTodo);
@@ -16,10 +23,10 @@ app.get('/todo/:todoId',todoController.getTodoById);
 
 
 // listening to request on localhost port 5010
-app.listen(5010,() => {
-    console.log("My server is Up and running ");
+app.listen(PORT,() => {
+    console.log("My server is Up and running on port :",PORT);
     // connecting to the database
-    mongoose.connect('mongodb+srv://sammy0288:sammy0288@cluster0.68gyj.mongodb.net/todo_db?retryWrites=true&w=majority')
+    mongoose.connect(process.env.DB_URL)
     .then(function(){
         console.log("DataBase is connected");
     })
